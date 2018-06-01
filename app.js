da11e8756070a37be988c6fdc6336ba028442dc7
Mahuton <bpossoupe@example.com>
2018-06-01 22:21:38 +0200

    updating app file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var tmdbclient = require('themoviedbclient');//The data base movie module
+var tmdbclient = require('themoviedbclient'); //The data base movie module
 var express = require('express')
 var Twit = require('twit');
 var TwitterBot = require('node-twitterbot').TwitterBot;
@@ -9,35 +9,35 @@ var movie_id = '',
     tweetedTab = [];
 let url = 'https://www.themoviedb.org/movie/';
 //Instanciation
-var tmdb = new tmdbclient({api_key: process.env.TMDB_API_KEY});
-  tmdb.call("/movie/upcoming", {})
-    .then(function (data) {
-      for( var property in data ){
-        for( var elt in data[property] ){
-          if (typeof data[property][elt].id != 'undefined') {
-            movie_id = data[property][elt].id;
-            movie_title = data[property][elt].title;
-            movie_title = movie_title.replace(/ /g, '-').replace(/'/g, '').replace(/,/g, '').toLowerCase();
-            link = url + movie_id + '-' + movie_title;
-            urlTab.push( link );
-          }
+var tmdb = new tmdbclient(process.env.TMDB_API_KEY);
+tmdb.call("/movie/upcoming", {})
+    .then(function(data) {
+        for (var property in data) {
+            for (var elt in data[property]) {
+                if (typeof data[property][elt].id != 'undefined') {
+                    movie_id = data[property][elt].id;
+                    movie_title = data[property][elt].title;
+                    movie_title = movie_title.replace(/ /g, '-').replace(/'/g, '').replace(/,/g, '').toLowerCase();
+                    link = url + movie_id + '-' + movie_title;
+                    urlTab.push(link);
+                }
+
+            }
 
         }
-
-      }
-      var Bot = new TwitterBot({
-       consumer_key: process.env.BOT_CONSUMER_KEY,
-       consumer_secret: process.env.BOT_CONSUMER_SECRET,
-       access_token: process.env.BOT_ACCESS_TOKEN,
-       access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
-      });
-      var movie = urlTab[ Math.floor( Math.random() * urlTab.length ) ];
-      tweetedTab.push(movie);
-      console.log(tweetedTab.length);
-      for (var i = 0; i <= tweetedTab.length; i++) {
-        if( movie != tweetedTab[i] ){
-          Bot.tweet('Hey! Here is another upcoming movie. Enjoy!' + '\n' + '@b_mahuton' + ' ' + '@themoviedb' + '\n' + movie);
-          console.log('tweeted');
+        var Bot = new TwitterBot({
+            consumer_key: process.env.BOT_CONSUMER_KEY,
+            consumer_secret: process.env.BOT_CONSUMER_SECRET,
+            access_token: process.env.BOT_ACCESS_TOKEN,
+            access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
+        });
+        var movie = urlTab[Math.floor(Math.random() * urlTab.length)];
+        tweetedTab.push(movie);
+        console.log(tweetedTab.length);
+        for (var i = 0; i <= tweetedTab.length; i++) {
+            if (movie != tweetedTab[i]) {
+                Bot.tweet('Hey! Here is another upcoming movie. Enjoy!' + '\n' + '@b_mahuton' + ' ' + '@themoviedb' + '\n' + movie);
+                console.log('tweeted');
+            }
         }
-      }
     });
